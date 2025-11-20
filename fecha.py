fd8ef701b7f916bfdbca007a9881f9b71c8566bd
William <ricardosierraluna@gmail.com>
2025-11-19 19:17:38 -0500

    Python 2

---
+++ fecha.py
@@ -0,0 +1,6 @@
+# fecha1  = int(input("Ingrese el numero que desea restar: "))
+# fecha2  = int(input("Ingrese el numero que desea restar 2: "))
+# fecha_unida = 
+# print(fecha1-fecha2)
+
+print(float(10/11/2025 - 19/11/2025))
